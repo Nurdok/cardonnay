e0092dae400a7ed3ee7dc3c881074e14512a6963
yoseft <yoseftwaik@gmail.com>
2020-11-01 01:57:47 +0200

    added allocation function

---
+++ game/utils.js
@@ -0,0 +1,23 @@
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+function genTeamAllocation(length) {
+    let teamAllocation = []
+    for (let i = 0; i < length; i++) {
+        if (i < length / 2) {
+            teamAllocation.push(0);
+        } else {
+            teamAllocation.push(1);
+        }
+    }
+    shuffleArray(teamAllocation);
+    return teamAllocation;
+}
+
+
